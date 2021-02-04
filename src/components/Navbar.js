a6b94a14d21498b14c1be5554ad0a7bd02d9cdc4
sp21 <pshreyasgowda1997@gmail.com>
2021-02-04 16:39:30 +0530

    modified some styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import { NavLink } from 'react-router-dom';
+
 function Navbar() {
     return (
-        <div>
-            <nav>
-                <NavLink exact to="/">Home</NavLink>
+        <div >
+            <nav >
+                <NavLink exact to="/" >Home</NavLink>
                 <NavLink exact to="/about">About</NavLink>
                 <NavLink exact to="/contact">Contact</NavLink>
             </nav>
