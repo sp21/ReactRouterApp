2a57b7b2286f01a786352e7f0b5a0f4e17eb0655
sp21 <pshreyasgowda1997@gmail.com>
2021-02-04 16:16:47 +0530

    adding routes to components

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <NavLink exact to="/">Home</NavLink>
+                <NavLink exact to="/about">About</NavLink>
+                <NavLink exact to="/contact">Contact</NavLink>
+            </nav>
+
+        </div>
+    );
+}
+
+export default Navbar;
